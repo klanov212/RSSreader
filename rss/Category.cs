4fbffd24f521c278a11f1d5b8291340d50f193bd
klanov212 <klara.nordstrom00@gmail.com>
2022-10-11 09:19:21 +0200

    Klasser tillagda

---
+++ rss/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rss
+{
+    internal class Category : IEdit
+    {
+        
+        //Interface metoderna
+        public void Delete()
+        {
+
+        }
+        public void ChangeName()
+        {
+
+        }
+        public void Add()
+        {
+
+        }
+    }
+}
---
+++ rss/IEdit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rss
+{
+    internal interface IEdit
+    {
+        void Delete();
+        void ChangeName();
+        void Add();
+    }
+}
